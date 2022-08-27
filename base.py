2c7e4f3ffa5468a582a46656ed340c564979115f
Varun Barad <varun@varunbarad.com>
2022-08-28 00:05:58 +0530

    Lesson 20

--- base.py
+++ base.py
@@ -103,6 +103,11 @@ def checkout(oid):
     data.set_HEAD(oid)
 
 
+def create_tag(name, oid):
+    # ToDo: Actually create the tag
+    pass
+
+
 Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
 
 
