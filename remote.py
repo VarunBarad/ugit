115345e67a26ae9b6cad82c55154fae6d1e519b9
Varun Barad <varun@varunbarad.com>
2022-09-19 19:31:21 +0530

    Lesson 58

--- remote.py
+++ remote.py
@@ -3,6 +3,10 @@ import data
 
 def fetch(remote_path):
     print('Will fetch the following refs:')
+    for ref_name, _ in _get_remote_refs(remote_path, 'refs/heads').items():
+        print(f'- {ref_name}')
+
+
+def _get_remote_refs(remote_path, prefix=''):
     with data.change_git_dir(remote_path):
-        for ref_name, _ in data.iter_refs('refs/heads'):
-            print(f'- {ref_name}')
+        return {ref_name: ref.value for ref_name, ref in data.iter_refs(prefix)}
