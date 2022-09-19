e171f87a861dc9baf7cefcb8d30d5d6d63df4dff
Varun Barad <varun@varunbarad.com>
2022-09-19 23:42:11 +0530

    Lesson 65

--- base.py
+++ base.py
@@ -295,13 +295,28 @@ def get_oid(name):
 
 
 def add(filenames):
-    with data.get_index() as index:
-        for filename in filenames:
-            # Normalize path
-            filename = os.path.relpath(filename)
-            with open(filename, 'rb') as f:
-                oid = data.hash_object(f.read())
+    def add_file(filename):
+        # Normalize path
+        filename = os.path.relpath(filename)
+        with open(filename, 'rb') as f:
+            oid = data.hash_object(f.read())
             index[filename] = oid
+
+    def add_directory(dir_name):
+        for root, _, filenames in os.walk(dir_name):
+            for filename in filenames:
+                # Normalize path
+                path = os.path.relpath(f'{root}/{filename}')
+                if is_ignored(path) or not os.path.isfile(path):
+                    continue
+                add_file(path)
+
+    with data.get_index() as index:
+        for name in filenames:
+            if os.path.isfile(name):
+                add_file(name)
+            elif os.path.isdir(name):
+                add_directory(name)
 
 
 def is_ignored(path):
